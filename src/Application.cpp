d647b902028939bd8414a5dceaaa521ca7b1ca31
k911 <kondzio9114@gmail.com>
2016-11-04 20:01:07 +0100

    Simple BMP loading

---
+++ src/Application.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include "SDL.h"
+#include "BMP_Surface.h"
+#include "Huffman.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string test_bmp;
+    // Initialize SDL
+    try
+    {
+		  SDL sdl(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    }
+    catch (const InitError &err)
+    {
+      cerr << "Error while initializing SDL:  " << err.what() << std::endl;
+      return 1;
+    }
+
+    if(argc == 3 && strcmp(argv[1], "-f") == 0) {
+      test_bmp = argv[2];
+    } else {
+      cout << "Podaj plik *.bmp do kompresji: "; cin >> test_bmp;
+    }
+
+    BMP_Surface *mSurface = new BMP_Surface();
+    if (!mSurface->loadBMP(test_bmp))
+    {
+      // Delete last 3 characters which stands for appname;
+      argv[0][strlen(argv[0])-3]= '\0';
+      cout << "BMP file: '" << test_bmp << "' could not be loaded! [Debug] called from: " << argv[0] << endl;
+      return 2;
+    }
+
+    Huffman *huff = new Huffman(mSurface);
+    huff->runHoffman();
+
+    // Clean
+    delete huff;
+    delete mSurface;
+
+    // Return sucess
+    return EXIT_SUCCESS;
+}
